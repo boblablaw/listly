a02b69ef41d732084b917d1729a391736d2d0abd
Jason Beatty <jasondavidbeatty@gmail.com>
2015-03-10 17:28:40 -0400

    Create task model

---
+++ js/task.js
@@ -0,0 +1,26 @@
+var Task = function() {
+	var self = this;
+	self.counter = 0;
+	
+	// write get set ID function here
+	function getOrSetId(id) {
+		if (!id) {
+			id = self.counter + 1;
+		}
+		incrementCounter(id);
+		return id;
+	}
+
+	function incrementCounter(id) {
+		if (id > self.counter) {
+			self.counter = id;
+		}
+	}
+
+	function Task(properties) { //constructor
+		this.name = properties.name;
+		this.id = getOrSetId(properties.id);
+	}
+
+	return Task;
+}();
